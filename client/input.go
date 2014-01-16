d93cfd6f1cf3ed3ad18634a4d453943d43aabc76
mortdeus <mortdeus@gocos2d.org>
2014-01-16 14:11:40 -0600

    moved seat type to input.go

--- client/input.go
+++ client/input.go
@@ -1,5 +1,17 @@
 package client
 
+type Seat struct{}
+
+func (s *Seat) Marshal() {}
+
 type Pointer struct{}
+
+func (p *Pointer) Marshal() {}
+
 type Keyboard struct{}
+
+func (k *Keyboard) Marshal() {}
+
 type Touch struct{}
+
+func (t *Touch) Marshal() {}
