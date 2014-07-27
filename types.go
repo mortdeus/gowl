fb8ba04d8f35eae29a92d1825f9e7db57dabb0d0
mortdeus <mortdeus@gocos2d.org>
2014-07-27 16:08:57 -0500

    implemented methods on String to satisfy GobEncoder/GobDecoder interfaces

--- types.go
+++ types.go
@@ -11,47 +11,53 @@ type (
 	Int    int32
 	Uint   uint32
 	Fixed  int32
-	String []byte
+	String string
 	Object uint32
 	NewID  uint32
 	Array  []byte
 	FD     uintptr
 )
 
-func StringEncode(s string) (String, error) {
-	b, err := syscall.ByteSliceFromString(s)
+func (s *String) GobEncode() ([]byte, error) {
+	buf := new(bytes.Buffer)
+
+	bs, err := syscall.ByteSliceFromString(string(*s))
 	if err != nil {
 		return nil, err
 	}
-	buf := new(bytes.Buffer)
-
-	if err := binary.Write(buf, binary.LittleEndian, uint32(len(b))); err != nil {
+	if err := binary.Write(buf, binary.LittleEndian, uint32(len(bs))); err != nil {
 		return nil, err
 	}
-	pad := (4 - (len(b) % 4)) &^ 04
-
-	if _, err := buf.Write(append(b, make([]byte, pad)...)); err != nil {
+	if err := binary.Write(buf, binary.LittleEndian, bs); err != nil {
 		return nil, err
 	}
-
-	return String(buf.Bytes()), nil
+	pad := (4 - (len(bs) % 4)) &^ 04
+	if err := binary.Write(buf, binary.LittleEndian, uint32(pad)); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
 }
+func (s1 *String) GobDecode(s []byte) error {
 
-func StringDecode(s String) (string, error) {
 	var slen uint32
-	if err := binary.Read(bytes.NewBuffer(s[:4]), binary.LittleEndian, &slen); err != nil {
-		return "", err
+	if err := binary.Read(bytes.NewReader(s[:4]), binary.LittleEndian, &slen); err != nil {
+		return err
 	}
-	return string(s[4 : (slen+4)-1]), nil
+	var bs = make([]byte, slen)
+	if err := binary.Read(bytes.NewReader(s[4:4+slen]), binary.LittleEndian, bs); err != nil {
+		return err
+	}
+	*s1 = String(bs[:len(bs)-1])
+	return nil
 }
 
-func FixedToF64(f Fixed) float64 {
+func (Fixed) ToFloat64(f Fixed) float64 {
 	d := int64(1023+44)<<52 + (1 << 51) + int64(f)
 	return *(*float64)(unsafe.Pointer(&d)) - float64(3<<43)
 }
-func FixedFromF64(d float64) Fixed {
+func (Fixed) FromFloat64(d float64) Fixed {
 	f := d + float64(uint64(3<<(51-8)))
 	return *(*Fixed)(unsafe.Pointer(&f))
 }
-func FixedToInt(f Fixed) int   { return int(f) / 256 }
-func FixedFromInt(i int) Fixed { return Fixed(i) * 256 }
+func (Fixed) ToInt32(f Fixed) int32   { return int32(f) / 256 }
+func (Fixed) FromInt32(i int32) Fixed { return Fixed(i) * 256 }
